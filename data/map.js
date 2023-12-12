d724ec74441dd515c6900a08e85fef3a5303bc32
pgm-stijwalr <147693656+pgm-stijwalr@users.noreply.github.com>
2023-12-12 15:49:30 +0100

    parcours update

---
+++ data/map.js
@@ -0,0 +1,27 @@
+const boxes = [
+    {
+        Y: "60.4%",
+        X: "68%",
+        img: "./static/img/05.jpg",
+    },
+    {
+        Y: "48%",
+        X: "56.5%",
+        img: "./static/img/04.jpg",
+    },
+    {
+        Y: "45%",
+        X: "41%",
+        img: "./static/img/03.jpg",
+    },
+    {
+        Y: "24%",
+        X: "37%",
+        img: "./static/img/02.jpg",
+    },
+    {
+        Y: "29%",
+        X: "15%",
+        img: "./static/img/01.jpg",
+    },
+];
---
+++ static/js/boxes.js
@@ -0,0 +1,20 @@
+(() => {
+    const $map = document.getElementById("circles");
+    const generateHTMLForBoxes = (boxes) => {
+        let html = "";
+        for (const box of boxes) {
+            html += `<div class="box" style="left:${box.X}; top:${box.Y};" data-src="${box.img}"></div>`;
+        }
+        return html;
+    };
+
+    const buildUI = () => {
+        $map.innerHTML = generateHTMLForBoxes(boxes);
+    };
+
+    const initialize = () => {
+        buildUI();
+    };
+
+    initialize();
+})();
